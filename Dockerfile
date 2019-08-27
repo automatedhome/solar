6d09d5460188e3cd82600723d83d3ee60123f0ed
paulfantom <pawel@krupa.net.pl>
2019-08-27 14:31:03 +0200

    configfile support

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,6 @@ RUN make build
 FROM arm32v7/busybox:1.30-glibc
 
 COPY --from=0 /go/src/github.com/automatedhome/solar/solar /usr/bin/solar
+COPY --form=0 /go/src/github.com/automatedhome/solar/config.yaml /config.yaml
 
 ENTRYPOINT [ "/usr/bin/solar" ]
--- cmd/main.go
+++ cmd/main.go
@@ -3,52 +3,56 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/url"
 	"strconv"
 	"time"
+
+	"gopkg.in/yaml.v2"
 
 	mqttclient "github.com/automatedhome/common/pkg/mqttclient"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
 type DataPoint struct {
-	val  float64
-	addr string
+	Val  float64 `yaml:"value,omitempty"`
+	Addr string  `yaml:"address"`
 }
 
 type Settings struct {
-	solarCritical DataPoint
-	solarOn       DataPoint
-	solarOff      DataPoint
-	tankMax       DataPoint
-	flow          struct {
-		dutyMin DataPoint
-		tempMin DataPoint
-		dutyMax DataPoint
-		tempMax DataPoint
+	SolarCritical DataPoint `yaml:"solarCritical"`
+	SolarOn       DataPoint `yaml:"solarOn"`
+	SolarOff      DataPoint `yaml:"solarOff"`
+	TankMax       DataPoint `yaml:"tankMax"`
+	Flow          struct {
+		DutyMin DataPoint `yaml:"dutyMin"`
+		TempMin DataPoint `yaml:"tempMin"`
+		DutyMax DataPoint `yaml:"dutyMax"`
+		TempMax DataPoint `yaml:"tempMax"`
 	} `yaml:"flow"`
 }
 
 type Sensors struct {
-	solarUp  DataPoint
-	solarIn  DataPoint
-	solarOut DataPoint
-	tankUp   DataPoint
+	SolarUp  DataPoint `yaml:"solarUp"`
+	SolarIn  DataPoint `yaml:"solarIn"`
+	SolarOut DataPoint `yaml:"solarOut"`
+	TankUp   DataPoint `yaml:"tankUp"`
 }
 
 type Actuators struct {
-	pump string `yaml:"pump"`
-	sw   string `yaml:"switch"`
-	flow string `yaml:"flow"`
+	Pump string `yaml:"pump"`
+	Sw   string `yaml:"switch"`
+	Flow string `yaml:"flow"`
 }
 
 type Config struct {
-	actuators Actuators `yaml:"actuators"`
-	sensors   Sensors   `yaml:"sensors"`
-	settings  Settings  `yaml:"settings"`
+	Actuators Actuators `yaml:"actuators"`
+	Sensors   Sensors   `yaml:"sensors"`
+	Settings  Settings  `yaml:"settings"`
 }
 
+var config Config
 var settings Settings
 var sensors Sensors
 var actuators Actuators
@@ -62,39 +66,39 @@ func onMessage(client mqtt.Client, message mqtt.Message) {
 		return
 	}
 	switch message.Topic() {
-	case sensors.solarUp.addr:
-		sensors.solarUp.val = value
-	case sensors.solarIn.addr:
-		sensors.solarIn.val = value
-	case sensors.solarOut.addr:
-		sensors.solarOut.val = value
-	case sensors.tankUp.addr:
-		sensors.tankUp.val = value
-	case settings.solarCritical.addr:
-		settings.solarCritical.val = value
-	case settings.solarOn.addr:
-		settings.solarOn.val = value
-	case settings.solarOff.addr:
-		settings.solarOff.val = value
-	case settings.tankMax.addr:
-		settings.tankMax.val = value
-	case settings.flow.dutyMin.addr:
-		settings.flow.dutyMin.val = value
-	case settings.flow.dutyMax.addr:
-		settings.flow.dutyMax.val = value
-	case settings.flow.tempMin.addr:
-		settings.flow.tempMin.val = value
-	case settings.flow.tempMax.addr:
-		settings.flow.tempMax.val = value
+	case sensors.SolarUp.Addr:
+		sensors.SolarUp.Val = value
+	case sensors.SolarIn.Addr:
+		sensors.SolarIn.Val = value
+	case sensors.SolarOut.Addr:
+		sensors.SolarOut.Val = value
+	case sensors.TankUp.Addr:
+		sensors.TankUp.Val = value
+	case settings.SolarCritical.Addr:
+		settings.SolarCritical.Val = value
+	case settings.SolarOn.Addr:
+		settings.SolarOn.Val = value
+	case settings.SolarOff.Addr:
+		settings.SolarOff.Val = value
+	case settings.TankMax.Addr:
+		settings.TankMax.Val = value
+	case settings.Flow.DutyMin.Addr:
+		settings.Flow.DutyMin.Val = value
+	case settings.Flow.DutyMax.Addr:
+		settings.Flow.DutyMax.Val = value
+	case settings.Flow.TempMin.Addr:
+		settings.Flow.TempMin.Val = value
+	case settings.Flow.TempMax.Addr:
+		settings.Flow.TempMax.Val = value
 	}
 }
 
 func stop(reason string) {
 	if circuitRunning {
 		log.Println("Stopping: " + reason)
-		client.Publish(actuators.pump, 0, false, "0")
-		client.Publish(actuators.sw, 0, false, "0")
-		client.Publish(actuators.flow, 0, false, fmt.Sprintf("%.2f", settings.flow.dutyMin.val))
+		client.Publish(actuators.Pump, 0, false, "0")
+		client.Publish(actuators.Sw, 0, false, "0")
+		client.Publish(actuators.Flow, 0, false, fmt.Sprintf("%.2f", settings.Flow.DutyMin.Val))
 		circuitRunning = false
 	}
 }
@@ -102,68 +106,39 @@ func stop(reason string) {
 func start() {
 	if !circuitRunning {
 		log.Println("Detected optimal conditions. Harvesting.")
-		client.Publish(actuators.pump, 0, false, "1")
-		client.Publish(actuators.sw, 0, false, "1")
+		client.Publish(actuators.Pump, 0, false, "1")
+		client.Publish(actuators.Sw, 0, false, "1")
 		circuitRunning = true
 	}
 }
 
 func calculateFlow() float64 {
 	// Flow function:
-	// ^ [flow]                        | s_min, ΔT <= T_min
-	// |                    flow(ΔT) = | A * ΔT + B, A = (s_max - s_min) / (T_max - T_min), B = s_min - T_min * A
+	// ^ [Flow]                        | s_min, ΔT <= T_min
+	// |                    Flow(ΔT) = | A * ΔT + B, A = (s_max - s_min) / (T_max - T_min), B = s_min - T_min * A
 	// |       -----------             | s_max, ΔT >= T_max
 	// |      /
 	// |     /
 	// |____/
 	// |                  [ΔT]
 	// +------------------->
-	delta := sensors.solarIn.val - sensors.solarOut.val
-	if delta <= settings.flow.tempMin.val {
-		return settings.flow.dutyMin.val
+	delta := sensors.SolarIn.Val - sensors.SolarOut.Val
+	if delta <= settings.Flow.TempMin.Val {
+		return settings.Flow.DutyMin.Val
 	}
-	if delta >= settings.flow.tempMax.val {
-		return settings.flow.dutyMax.val
+	if delta >= settings.Flow.TempMax.Val {
+		return settings.Flow.DutyMax.Val
 	}
-	// flow(ΔT) = a * ΔT + b
-	a := (settings.flow.dutyMax.val - settings.flow.dutyMin.val) / (settings.flow.tempMax.val - settings.flow.tempMin.val)
-	b := settings.flow.dutyMin.val - settings.flow.tempMin.val*a
+	// Flow(ΔT) = a * ΔT + b
+	a := (settings.Flow.DutyMax.Val - settings.Flow.DutyMin.Val) / (settings.Flow.TempMax.Val - settings.Flow.TempMin.Val)
+	b := settings.Flow.DutyMin.Val - settings.Flow.TempMin.Val*a
 	return a*delta + b
-}
-
-func init() {
-	// TODO read it from yaml file
-	settings = Settings{}
-	sensors = Sensors{}
-	actuators = Actuators{}
-	// TODO uncomment after acceptance testing
-	//actuators.flow = "solar/actuators/flow" // proxy to "evok/ao/1/set" and later to flow-controller
-	//actuators.pump = "solar/actuators/pump" // proxy to "evok/relay/3/set"
-	//actuators.sw = "solar/actuators/switch" // proxy to "evok/relay/2/set"
-	actuators.flow = "testing/solar/actuators/flow" // proxy to "evok/ao/1/set"
-	actuators.pump = "testing/solar/actuators/pump" // proxy to "evok/relay/3/set"
-	actuators.sw = "testing/solar/actuators/switch" // proxy to "evok/relay/2/set"
-
-	sensors.solarIn = DataPoint{300, "solar/temperature/in"}
-	sensors.solarOut = DataPoint{300, "solar/temperature/out"}
-	sensors.solarUp = DataPoint{300, "solar/temperature/up"}
-	sensors.tankUp = DataPoint{300, "tank/temperature/up"}
-
-	settings.solarCritical = DataPoint{90, "solar/settings/critical"}
-	settings.solarOn = DataPoint{8, "solar/settings/on"}
-	settings.solarOff = DataPoint{5, "solar/settings/off"}
-	settings.tankMax = DataPoint{65, "solar/settings/tank"}
-
-	settings.flow.tempMin = DataPoint{5, "solar/settings/flow/t_min"}
-	settings.flow.tempMax = DataPoint{9, "solar/settings/flow/t_max"}
-	settings.flow.dutyMin = DataPoint{1.8, "solar/settings/flow/d_min"} // min 0
-	settings.flow.dutyMax = DataPoint{3, "solar/settings/flow/d_max"}   // max 10
-
 }
 
 func main() {
 	broker := flag.String("broker", "tcp://127.0.0.1:1883", "The full url of the MQTT server to connect to ex: tcp://127.0.0.1:1883")
-	clientID := flag.String("clientid", "solar", "A clientid for the connection")
+	clientID := flag.String("clientid", "Solar", "A clientid for the connection")
+	configFile := flag.String("config", "/config.yaml", "Provide configuration file with MQTT topic mappings")
 	flag.Parse()
 
 	brokerURL, err := url.Parse(*broker)
@@ -171,17 +146,41 @@ func main() {
 		log.Fatal(err)
 	}
 
+	log.Printf("Reading configuration from %s", *configFile)
+	data, err := ioutil.ReadFile(*configFile)
+	if err != nil {
+		log.Fatalf("File reading error: %v", err)
+		return
+	}
+
+	err = yaml.UnmarshalStrict(data, &config)
+	//err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	settings = config.Settings
+	actuators = config.Actuators
+	sensors = config.Sensors
+	// set initial sensors values and ignore ones provided by config file
+	// this is used as a locking mechanism to prevent starting control loop without current sensors data
+	lockTemp := 300.0
+	sensors.SolarUp.Val = lockTemp
+	sensors.SolarIn.Val = lockTemp
+	sensors.SolarOut.Val = lockTemp
+	sensors.TankUp.Val = lockTemp
+
 	// subscribe to topics
 	var topics []string
-	topics = append(topics, sensors.solarIn.addr, sensors.solarOut.addr, sensors.solarUp.addr, sensors.tankUp.addr)
-	topics = append(topics, settings.solarCritical.addr, settings.solarOn.addr, settings.solarOff.addr, settings.tankMax.addr)
-	topics = append(topics, settings.flow.tempMin.addr, settings.flow.tempMax.addr, settings.flow.dutyMin.addr, settings.flow.dutyMax.addr)
+	topics = append(topics, sensors.SolarIn.Addr, sensors.SolarOut.Addr, sensors.SolarUp.Addr, sensors.TankUp.Addr)
+	topics = append(topics, settings.SolarCritical.Addr, settings.SolarOn.Addr, settings.SolarOff.Addr, settings.TankMax.Addr)
+	topics = append(topics, settings.Flow.TempMin.Addr, settings.Flow.TempMax.Addr, settings.Flow.DutyMin.Addr, settings.Flow.DutyMax.Addr)
 	client = mqttclient.New(*clientID, brokerURL, topics, onMessage)
 	log.Printf("Connected to %s as %s and waiting for messages\n", *broker, *clientID)
 
 	// Wait for sensors data
 	for {
-		if sensors.solarIn.val != 300 && sensors.solarOut.val != 300 && sensors.solarUp.val != 300 && sensors.tankUp.val != 300 {
+		if sensors.SolarIn.Val != lockTemp && sensors.SolarOut.Val != lockTemp && sensors.SolarUp.Val != lockTemp && sensors.TankUp.Val != lockTemp {
 			break
 		}
 		log.Println("Waiting 15s for sensors data...")
@@ -197,45 +196,45 @@ func main() {
 	for {
 		time.Sleep(1 * time.Second)
 
-		if sensors.solarUp.val >= settings.solarCritical.val {
-			stop("Critical solar temperature reached")
+		if sensors.SolarUp.Val >= settings.SolarCritical.Val {
+			stop("Critical Solar Temperature reached")
 			continue
 		}
 
-		if sensors.solarOut.val >= sensors.solarUp.val {
-			stop("Heat escape prevention (Tout >= Tsolar)")
+		if sensors.SolarOut.Val >= sensors.SolarUp.Val {
+			stop("Heat escape prevention (Tout >= TSolar)")
 			continue
 		}
-		if sensors.tankUp.val > settings.tankMax.val {
+		if sensors.TankUp.Val > settings.TankMax.Val {
 			stop("Tank filled with hot water")
 			continue
 		}
 
-		if sensors.solarUp.val >= sensors.solarIn.val {
-			delta = (sensors.solarUp.val+sensors.solarIn.val)/2 - sensors.solarOut.val
+		if sensors.SolarUp.Val >= sensors.SolarIn.Val {
+			delta = (sensors.SolarUp.Val+sensors.SolarIn.Val)/2 - sensors.SolarOut.Val
 		} else {
-			delta = sensors.solarUp.val - sensors.solarOut.val
+			delta = sensors.SolarUp.Val - sensors.SolarOut.Val
 		}
 
-		if delta >= settings.solarOff.val {
-			if sensors.solarUp.val-sensors.solarOut.val > settings.solarOn.val {
+		if delta >= settings.SolarOff.Val {
+			if sensors.SolarUp.Val-sensors.SolarOut.Val > settings.SolarOn.Val {
 				start()
 			}
-			flow := calculateFlow()
-			if flow != lastFlow {
-				client.Publish(actuators.flow, 0, false, fmt.Sprintf("%.2f", flow))
-				lastFlow = flow
+			Flow := calculateFlow()
+			if Flow != lastFlow {
+				client.Publish(actuators.Flow, 0, false, fmt.Sprintf("%.2f", Flow))
+				lastFlow = Flow
 			}
 			reducedTill = time.Now().Add(30 * time.Minute)
 		} else if time.Now().Before(reducedTill) {
-			// Reduced heat exchange. Set flow to minimal value.
+			// Reduced heat exchange. Set Flow to minimal value.
 			if !reducedSent {
 				log.Println("Entering reduced heat exchange mode.")
-				client.Publish(actuators.flow, 0, false, fmt.Sprintf("%.2f", settings.flow.dutyMin.val))
+				client.Publish(actuators.Flow, 0, false, fmt.Sprintf("%.2f", settings.Flow.DutyMin.Val))
 				reducedSent = true
 			}
 		} else {
-			// Delta solarIn - solarOut is too low.
+			// Delta SolarIn - SolarOut is too low.
 			reducedSent = false
 			stop("In-Out delta too low.")
 		}
